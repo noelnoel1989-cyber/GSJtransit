87fbb82b61b0643a96167280810b87f1e6a9e30a
noelnoel1989-cyber <noelnoel1989@gmail.com>
2026-05-17 23:51:50 -0400

    Refactor transit.js by removing handler function
    Removed the handler function and added package metadata.

--- netlify/functions/transit.js
+++ netlify/functions/transit.js
@@ -1,30 +1,21 @@
-export async function handler() {
-  const url =
-    "https://api-endpoint.example-gtfs-proxy.com/nyc/subway-and-bus";
-
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    // normalize into your format
-    const results = (data || []).map(d => ({
-      line: d.line,
-      mins: d.minutes
-    }));
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(results)
-    };
-
-  } catch (e) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify([])
-    };
-  }
+{
+  "name": "gsj-transit",
+  "version": "1.0.0",
+  "description": "NYC Transit Dashboard for 1, B, C lines at 86th Street",
+  "main": "netlify/functions/transit.js",
+  "scripts": {
+    "start": "netlify dev"
+  },
+  "dependencies": {
+    "gtfs-realtime-bindings": "^0.4.0"
+  },
+  "devDependencies": {},
+  "keywords": [
+    "transit",
+    "mta",
+    "nyc",
+    "subway"
+  ],
+  "author": "",
+  "license": "MIT"
 }
